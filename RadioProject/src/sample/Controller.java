a37493d9baba2fb7199398a830fc4a42ca69eb07
Brayden Stewart <misterbms@hotmail.com>
2016-02-28 16:55:11 -0800

    Initial commit with the JavaFX application and simple button UI

---
+++ RadioProject/src/sample/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    public Button bBrayTest;
+
+    public void handlebBrayTest_Click()
+    {
+        System.out.println("Test");
+    }
+}
